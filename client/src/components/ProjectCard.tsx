162c28b8607c99abcb5bfe90b49cf5a6ad2c1ea1
@Masum0895 <2221793@iub.edu.bd>
2026-02-22 04:23:50 +0600

    Downloadimage&video,share,Delete,publish feature added on my generated page

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -2,13 +2,21 @@ import React, { useState } from "react"
 import type {Project} from "../types"
 import { useNavigate } from "react-router-dom"
 
-import { Loader2Icon } from "lucide-react";
+import { EllipsisIcon, ImageIcon, Loader2Icon, PlaySquareIcon, Share2Icon, Trash2Icon } from "lucide-react";
 import { img } from "framer-motion/client";
 
 const ProjectCard = ({gen, setGenerations, forCommunity = false} : {gen: Project, setGenerations: React.Dispatch<React.SetStateAction<Project[]>>, forCommunity?:boolean}) => {
 
     const navigate = useNavigate();
     const [menuOpen, setMenuOpen] = useState(false)
+    const handleDelete = async(id:string) =>{
+        const confirm = window.confirm("Are you sure you want to delete this project?");
+        if(!confirm) return;
+        console.log(id)
+    }
+    const togglePublish = async(projectId:string) =>{
+        console.log(projectId)
+    }
   return (
     <div key={gen.id} className="mb-4 break-inside-avoid">
         <div className="bg-white/5 border border-white/10 rounded-xl overflow-hidden hover:border-white/20 transition group">
@@ -36,6 +44,22 @@ const ProjectCard = ({gen, setGenerations, forCommunity = false} : {gen: Project
                     )}
                     
                 </div>
+                {/* Action Menu only for my generation page*/}
+                {!forCommunity && (
+                    <div className="absolute right-3 top-3 sm:opacity-0 group-hover:opacity-100 transition flex items-center gap-2">
+                        <div onMouseDownCapture={()=>{setMenuOpen(true)}} onMouseLeave={()=>{setMenuOpen(false)}}className="absolute top-3 right-3">
+                            <EllipsisIcon className="ml-auto bg-black/10 rounded-full p-1 size-7"/>
+                        </div>
+                        <div className="flex flex-col items-end w-32 text-sm">
+                            <ul className={`text-xs ${menuOpen ? 'block' : 'hidden'} verflow-hidden right-0 peer-focus:block hover:block w-40 bg-black/50 backdrop-blur text-white border border--gray-500/50 rounded-lg shadow-md mt-2 py-1 z-10`}>
+                                {gen.generatedImage && <a href="#" download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer"><ImageIcon size={14}/> Download Image</a>}
+                                {gen.generatedVideo && <a href="#" download className="flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer"><PlaySquareIcon size={14}/> Download Video</a>}
+                                {(gen.generatedVideo || gen.generatedImage) && <button onClick={()=> navigator.share({url: gen.generatedImage || gen.generatedVideo,title:gen.productName, text: gen.productDescription})} className="w-full flex gap-2 items-center px-4 py-2 hover:bg-black/10 cursor-pointer"><Share2Icon size={14}/>Share</button>}
+                                <button onClick={()=> handleDelete(gen.id)} className="w-full flex gap-2 items-center px-4 py-2 hover:bg-red-950/10 text-red-400 cursor-pointer"><Trash2Icon size={14}/>Delete</button>
+                            </ul>
+                        </div>
+                    </div>
+                )}
                 {/* source images */}
                 <div className="absolute right-3 bottom-3">
                     <img src={gen.uploadedImages[0]} alt="product" className="w-16 h-16 object-cover rounded-full animate-float" />
